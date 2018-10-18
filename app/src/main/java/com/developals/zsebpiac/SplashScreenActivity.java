e8ad436716ced01c678020b78764c7c482f1ce94
Laura <palagyilaura@gmail.com>
2018-10-19 00:33:08 +0200

    fullscreen splash, choose layout changed

--- app/src/main/java/com/developals/zsebpiac/SplashScreenActivity.java
+++ app/src/main/java/com/developals/zsebpiac/SplashScreenActivity.java
@@ -3,6 +3,8 @@ package com.developals.zsebpiac;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,8 +12,13 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
         StartAnimations();
+
     }
 
     private void StartAnimations() {
